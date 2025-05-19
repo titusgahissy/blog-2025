b785f0ac1e2e9ecc1d61203c0e5e40cc1a9fb360
Titus <titus@gahissy.com>
2025-05-19 16:58:23 +0100

    Update intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <Container className="pt-12 md:pt-24">
       <HeroTitle>The Product<br />Is The Model</HeroTitle>
       <div className="text-xl md:text-5xl/13 font-normal pt-4">
-        Hello, I'm <span className="font-medium">Titus Gahissy</span>.<br />I explore not so boring ideas and<br />help teams and founders achieve more.
+        Hello, I'm <span className="font-medium">Titus Gahissy</span>.<br />I explore not so boring ideas and<br />challenge teams and founders to achieve more.
       </div>
       <div className="text-ld md:text-2xl pt-4 ">
         This is my little corner of the internet where I share<br />thoughts, travel moments, books and projects.
